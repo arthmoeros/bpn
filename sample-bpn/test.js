647cfe3fb4ef8061f07b66cda0e2413bac0d6a24
arthmoeros <artu.saavedra@gmail.com>
2018-07-11 00:08:44 -0400

    testing dump of process

--- sample-bpn/test.js
+++ sample-bpn/test.js
@@ -3,4 +3,19 @@ const fs = require('fs');
 
 let struc = yaml.load(fs.readFileSync('./sample-bpn/main.yml'));
 
-console.log(JSON.stringify(struc));
+let todo = [];
+
+for (let sectionName in struc.main) {
+    for(let i = 0; i < struc.main[sectionName].tasks.length; i+=1){
+        let item = struc.main[sectionName].tasks[i];
+        for(let taskName in item){          
+            todo.push({
+                sectionName,
+                taskName,
+                params: item[taskName]
+            });
+        }
+    }
+}
+
+console.log(JSON.stringify(todo));
